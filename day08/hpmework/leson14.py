a1b8c6c608c1e413cc52f4ad589dc4bdea984a48
SANDrex14 <sandrochapidze990@gmail.com>
2024-02-29 08:36:11 -0800

    commit

--- day08/hpmework/leson14.py
+++ day08/hpmework/leson14.py
@@ -1,16 +1,18 @@
+#2) მომხმარებელს შეეკითხეთ მისი ასაკი, ასევე შეეკითხეთ რამდენი წლით სურს დროში მოგზაურობა და დაპრინტეთ რამდენი წლის იქნება დროში მოგზაურობის შემდეგ
+
 print("gamarzoba dagismev ramdenime kitxvas da gameci pasuxi")
-user_kargi = input("kargi ")
+user_kargi = input("kargi? ")
 x = input("ramdeni wlis xar ")
 
 print("ramdeni wlit win waxvidodi momavalsi")
 y = input("drois manqanit ")
 
 print("gainteresebs ramdeni wlis iqnebi momavalsi mogzaurobit?")
-print("sen iqnebi")
+user_kargi = input("ki? ")
+print("shen iqnebi")
 
 a = int(x) + int(y)
-
 print(a + 0)
 
-
-
+user_komentari = input("ras fiqrob rogoria? ")
+print("madloba sheqebistvis :) ")
