dc494cf95855e736bc7440c95015635d89af2238
SANDrex14 <sandrochapidze990@gmail.com>
2024-03-05 10:35:04 -0800

    commit

--- day09/homework/leson16.py
+++ day09/homework/leson16.py
@@ -1,6 +1,5 @@
 #მომხმარებელს შემოატანინეთ ორი რიცხვი, მისი ასაკი და მშობლის ასაკი, შემდეგ კი შეადარეთ ისინი ერთმანეთს
 
-
 print("gamargoba mainteresebs romeli mshobeli upro didia deda tu mama?")
 user_age1 = input("ramdeni wlisaa dedaseni? ")
 user_age2 = input("ramdeni wlisaa mamaseni? ")
