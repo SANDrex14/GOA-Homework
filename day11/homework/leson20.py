911298483365f275b1c27f14993b355f9b5e71cc
SANDrex14 <sandrochapidze990@gmail.com>
2024-03-09 04:53:37 -0800

    commit

--- day11/homework/leson20.py
+++ day11/homework/leson20.py
@@ -2,17 +2,19 @@
 #        დაგვიბეჭდოს რომ დედა დიდი მამაზე, თუ პირიქით მამის ასაკი მეტი იქნება დედის ასაკზე მაგ შემთხვევაში დაგვიბეჭდოს 
 #        რომ მამა დიდია დედაზე. მინიშნება დაგჭირდებათ (if)
 
-print("me sen getyvi romelia ufro didi mama tu deda")
+print("tu gainteresebs dadasheni ufro didia tu mamasheni")
+
+print("upasuxe qvemot mocemul kitxvebs:) ")
+user_kitxva = input("tanaxma xar: ")
 
 user_age1 = input("semoiyvane dedis asaki: ")
 user_age2 = input("semoiyvane mamis asaki: ")
 
 if user_age1 > user_age2:
-    print("deda didia mamaze")
+    print("dedasheni ufro didia mamashenze")
 
 if user_age1 < user_age2:
-    print("mama didia dedaze")
-
+    print("mamasheni ufro didia dedashenze")
 
 
 
