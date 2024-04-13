6f6c0e8a7863cab0e145bec2a094740c89ef6af8
SANDrex14 <sandrochapidze990@gmail.com>
2024-04-13 12:04:32 -0700

    “commit”

--- day20/classwork/leson40.py
+++ day20/classwork/leson40.py
@@ -14,13 +14,13 @@
 #print("goa best")
 
 #i = iteration
-#for i in range(10, 21, 4):
-print(i)
-print(str(i) + ") I Love You ❤️")
-#for i in range(20, 100, 2):#ლუწი
-print(i)
-#for i in range(21, 100, 2):#კენტი
-print(i)
+# #for i in range(10, 21, 4):
+# print(i)
+# print(str(i) + ") I Love You ❤️")
+# #for i in range(20, 100, 2):#ლუწი
+# print(i)
+# #for i in range(21, 100, 2):#კენტი
+# print(i)
 for i in range(20, 100, 2):
     print(str(i) + ") even")
     print(str(i + 1) + ") odd")
