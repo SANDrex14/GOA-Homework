1d5a5986c03e494ee1eae41d90dcd2d073ece38a
SANDrex14 <sandrochapidze990@gmail.com>
2024-04-18 08:24:05 -0700

    commit

---
+++ day22/claswork/leson49.py
@@ -0,0 +1,43 @@
+
+
+# for i in range(2)
+#     print(i)
+
+
+#   while loop
+
+#   სანამ 5 > 2ზე
+#   while 5 > 2
+
+
+# i = 0
+# name = "sandro"
+# while name == "gabreli":
+#     print("hello")
+
+
+#  iteration  varieibl 
+
+
+
+# for i in range(2):
+#     print(i)
+
+
+#  i += 1
+#  i  = 1
+
+
+
+# spaces = "  "
+
+# for i in range(50):
+#     print(spaces, i)
+    
+#     spaces += '  '
+   
+
+
+
+
+
---
+++ day22/claswork/leson50.py
@@ -0,0 +1,11 @@
+user_akademia = input("romeli akademiaa saqartvelosi sauketeso: ")
+
+
+
+
+while user_akademia != "GOA ":
+    user_akademia = input("romeli akademiaa saqartvelosi sauketeso: ")
+
+print("GOA best")
+
+ 
