ae4bc6ce305c8ca01d3bbdb8d31fbf54476a5ae5
SANDrex14 <sandrochapidze990@gmail.com>
2024-04-23 12:40:56 -0700

    commit

---
+++ day24/claswork/leson55.py
@@ -0,0 +1,9 @@
+user_number= input("Shemoitane ricxvi romelic gaiyofa 5 ze MAGRAM MAQSIMUM RICXVI IYOS 40 ZE NAKLEBI: ")
+
+
+while user_number % "5"  != 0  :
+    user_number = int(input("ar iyofa 5 ze: "))
+
+print("yocag")
+
+ 
---
+++ day24/claswork/leson56.py
@@ -0,0 +1,12 @@
+password = input("password")
+
+name = input("enter you name: ")
+password_input = input("enter your password")
+
+while password != password_input:
+    password_input = input("enter you name")
+
+
+
+
+print("Good")
