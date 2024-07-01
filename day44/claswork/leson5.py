c6392709114944b60c3aa77a308f8cb48716c41f
SANDrex14 <sandrochapidze990@gmail.com>
2024-07-01 12:43:55 -0700

    commit

---
+++ day44/claswork/leson5.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(num):
+    sum += i
+
+print("The sum of numbers from 1 to", num, "is", sum)
---
+++ day44/homework/leson14.py
@@ -0,0 +1,6 @@
+age = 16
+
+if age >= 13 and age <= 19:
+    print("You are a teenager!")
+else:
+    print("You are not a teenager.")
